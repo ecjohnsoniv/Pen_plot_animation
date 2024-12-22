e97a7f850ae447b9cf2b721083c4a834171e572a
Christopher <Christopher@Xopher.io>
2024-12-22 00:06:48 -0600

    I fliped the dimentions on the grid line. The frames are now layed down correctly, I show this with a virtial line that is 480x640 and they line up across rows.

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -3,8 +3,9 @@
 vpype \
 eval "files=glob('frames/*.svg')" \
 eval "cols=6; rows=ceil(len(files)/cols)" \
-grid -o 10cm 15cm "%cols%" "%rows%" \
+grid -o 640px 480px "%cols%" "%rows%" \
     read --no-fail "%files[_i] if _i < len(files) else ''%" \
+    line 0 0 640px 480px \
 end \
 write combined.svg
 

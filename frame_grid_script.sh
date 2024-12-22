50d69fd072507e8f8bb3c234ac2ff22e1aa77200
Christopher <Christopher@Xopher.io>
2024-12-21 23:13:05 -0600

    This fixes fixes the scaling to geom. problem

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -4,7 +4,7 @@ vpype \
 eval "files=glob('frames/*.svg')" \
 eval "cols=6; rows=ceil(len(files)/cols)" \
 grid -o 10cm 15cm "%cols%" "%rows%" \
-read --no-fail "%files[_i] if _i < len(files) else ''%" \
-layout -m 0.5cm 10x15cm end \
+    read --no-fail "%files[_i] if _i < len(files) else ''%" \
+end \
 write combined.svg
 

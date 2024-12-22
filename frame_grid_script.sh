7ea64ef29d0192d8d53a742ce8c541988a2deef4
Christopher <Christopher@Xopher.io>
2024-12-22 16:25:58 -0600

    The each frame has a box arround it and there is padding between each frame. However, there is no padding on the top or the left side of the sheet

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -3,7 +3,7 @@
 vpype \
 eval "files=glob('frames/*.svg')" \
 eval "cols=6; rows=ceil(len(files)/cols)" \
-grid -o 640px 480px "%cols%" "%rows%" \
+grid -o 650px 490px "%cols%" "%rows%" \
     read --no-fail "%files[_i] if _i < len(files) else ''%" \
     line 0 0 640px 0 \
     line 640px 0 640px 480px \

ee6e18ec78776b6a3c2ee4c6cbba8bc390307edf
Christopher <Christopher@Xopher.io>
2024-12-22 00:28:11 -0600

    I framed each frame with a box, deleted the X

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -5,7 +5,10 @@ eval "files=glob('frames/*.svg')" \
 eval "cols=6; rows=ceil(len(files)/cols)" \
 grid -o 640px 480px "%cols%" "%rows%" \
     read --no-fail "%files[_i] if _i < len(files) else ''%" \
-    line 0 0 640px 480px \
+    line 0 0 640px 0 \
+    line 640px 0 640px 480px \
+    line 640px 480px 0 480px \
+    line 0 480px 0 0 \
 end \
 write combined.svg
 

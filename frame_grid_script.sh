0fe83beb8ff26f2445d85cee4b7596cd09c86075
Christopher <Christopher@Xopher.io>
2024-12-28 17:07:21 -0600

    softcoded dimentions of box as variables as well as the units

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+frameWidth='640'
+frameHeight='480'
+unit='px'
+
 vpype \
 eval "files=glob('frames/*.svg')" \
 eval "cols=6; rows=ceil(len(files)/cols)" \
 grid -o 650px 490px "%cols%" "%rows%" \
     read --no-fail "%files[_i] if _i < len(files) else ''%" \
-    line 0 0 640px 0 \
-    line 640px 0 640px 480px \
-    line 640px 480px 0 480px \
-    line 0 480px 0 0 \
+    line 0 0 ${frameWidth}${units} 0 \
+    line ${frameWidth}${units} 0 ${frameWidth}${units} ${frameHeight}${units} \
+    line ${frameWidth}${units} ${frameHeight}${units} 0 ${frameHeight}${units} \
+    line 0 ${frameHeight}${units} 0 0 \
 end \
 write combined.svg
 

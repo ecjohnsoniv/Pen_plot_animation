618a688939dd0cc5c66718f1ef0d7d3b785d44a4
Christopher <Christopher@Xopher.io>
2024-12-28 17:43:21 -0600

    I added the name of each frame to the frame using pytons oa.path.basename function. It is hackie at best to mix python and shell syntax in a bash script but it works for now. It should be noted the frames are all still out of order

--- frame_grid_script.sh
+++ frame_grid_script.sh
@@ -14,6 +14,7 @@ grid -o $((frameWidth + padding))${unit} $((frameHeight + padding))${unit} "%col
     line ${frameWidth}${units} 0 ${frameWidth}${units} ${frameHeight}${units} \
     line ${frameWidth}${units} ${frameHeight}${units} 0 ${frameHeight}${units} \
     line 0 ${frameHeight}${units} 0 0 \
+    text -p 5 15 "$(basename '%files[_i]%')" \
 end \
 write combined.svg
 
